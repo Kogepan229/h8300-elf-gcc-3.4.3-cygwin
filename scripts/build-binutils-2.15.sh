04950c0b89513ab0e94ae02eaab7e4ff10bfdebc
Kogepan25629 <kogepan25629@gmail.com>
2024-01-14 21:59:46 +0900

    test

--- scripts/build-binutils-2.15.sh
+++ scripts/build-binutils-2.15.sh
@@ -10,6 +10,8 @@ cd build/binutils-2.15
 # export CFLAGS="-w"
 export CFLAGS="-D_FORTIFY_SOURCE=0 -w"
 
+sh ../../src/binutils-2.15/config.guess
+
 ../../src/binutils-2.15/configure --host=x86_64-w64-mingw32 --prefix=/usr/local --target=h8300-elf || (echo status:$?; exit 1)
 make -j8 || (echo status:$?; exit 2)
 make install MAKEINFO=true || (echo status:$?; exit 3)
