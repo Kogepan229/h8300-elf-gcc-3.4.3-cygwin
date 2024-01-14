bfca02281589856b745f7ebf407850edf80fa02c
Kogepan25629 <kogepan25629@gmail.com>
2024-01-14 19:01:39 +0900

    Add -w

--- scripts/build-binutils-2.15.sh
+++ scripts/build-binutils-2.15.sh
@@ -7,6 +7,7 @@ fi
 mkdir -p build/binutils-2.15
 cd build/binutils-2.15
 
+export CFLAGS="-w"
 # export CFLAGS="-D_FORTIFY_SOURCE=0"
 
 ../../src/binutils-2.15/configure --host=x86_64-w64-mingw32 --prefix=/usr/local --target=h8300-elf || (echo status:$?; exit 1)
